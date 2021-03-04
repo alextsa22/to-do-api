6beef6bb124b471f550075a53fc9a6b7eacc6647
Alex TSA <alextsa22@gmail.com>
2021-03-04 20:26:34 +0600

    add services and repos

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"github.com/alextsa22/to-do-api/pkg/handler"
+	"github.com/alextsa22/to-do-api/pkg/repository"
 	"github.com/alextsa22/to-do-api/pkg/server"
+	"github.com/alextsa22/to-do-api/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(server.Server)
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,9 +1,16 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/alextsa22/to-do-api/pkg/service"
+	"github.com/gin-gonic/gin"
+)
 
 type Handler struct {
+	services *service.Service
+}
 
+func NewHandler(services *service.Service) *Handler {
+	return &Handler{services: services}
 }
 
 func (h *Handler) InitRoutes() *gin.Engine {
