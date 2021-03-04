491267ef6d4744efb867764c90341754d68ec239
Alex TSA <alextsa22@gmail.com>
2021-03-04 20:46:40 +0600

    add configs

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,27 @@ import (
 	"github.com/alextsa22/to-do-api/pkg/repository"
 	"github.com/alextsa22/to-do-api/pkg/server"
 	"github.com/alextsa22/to-do-api/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing confings: %s", err)
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(server.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while  running http server: %s", err)
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
