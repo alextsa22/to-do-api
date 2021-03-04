c168c981be5e4038ff633651f0e8ccdf2df13215
Alex TSA <alextsa22@gmail.com>
2021-03-04 20:12:28 +0600

    add handlers

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"github.com/alextsa22/to-do-api/pkg/handler"
 	"github.com/alextsa22/to-do-api/pkg/server"
 	"log"
 )
 
 func main() {
+	handlers := new(handler.Handler)
+
 	srv := new(server.Server)
-	if err := srv.Run("8080"); err != nil {
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while  running http server: %s", err)
 	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/alextsa22/to-do-api
 
 go 1.15
+
+require github.com/gin-gonic/gin v1.6.3
