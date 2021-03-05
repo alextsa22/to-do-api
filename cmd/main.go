2752bf3cdb7ff9b10130554d352e7112cbba35c4
Alex TSA <alextsa22@gmail.com>
2021-03-05 18:51:54 +0600

    add postgres db to the project

--- cmd/main.go
+++ cmd/main.go
@@ -5,8 +5,11 @@ import (
 	"github.com/alextsa22/to-do-api/pkg/repository"
 	"github.com/alextsa22/to-do-api/pkg/server"
 	"github.com/alextsa22/to-do-api/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
 func main() {
@@ -14,7 +17,23 @@ func main() {
 		log.Fatalf("error initializing confings: %s", err)
 	}
 
-	repos := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err)
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err)
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.15
 require (
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/gin-gonic/gin v1.6.3
+	github.com/jmoiron/sqlx v1.3.1
+	github.com/joho/godotenv v1.3.0
+	github.com/lib/pq v1.2.0
 	github.com/magiconair/properties v1.8.4 // indirect
 	github.com/mitchellh/mapstructure v1.4.1 // indirect
 	github.com/pelletier/go-toml v1.8.1 // indirect
