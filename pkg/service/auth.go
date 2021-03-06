fba4e3c0c34adaa30dd5eb0d243984f9e6fda6b8
Alex TSA <alextsa22@gmail.com>
2021-03-06 22:07:41 +0600

    implement sign-in handler

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -5,11 +5,20 @@ import (
 	"fmt"
 	"github.com/alextsa22/to-do-api/pkg/model"
 	"github.com/alextsa22/to-do-api/pkg/repository"
+	"github.com/dgrijalva/jwt-go"
+	"time"
 )
 
 const (
-	salt = "ahdga24wnefmb2535iuhrg"
+	salt       = "ahdga24wnefmb2535iuhrg"
+	signingKey = "sdjf#fsjfLJFaj88r53fs"
+	tokenTTL   = 5 * time.Minute
 )
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -20,12 +29,29 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 }
 
 func (s *AuthService) CreateUser(user model.User) (int, error) {
-	user.Password = s.generatePasswordHash(user.Password)
+	user.Password = generatePasswordHash(user.Password)
 
 	return s.repo.CreateUser(user)
 }
 
-func (s *AuthService) generatePasswordHash(password string) string {
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
+}
+
+func generatePasswordHash(password string) string {
 	hash := sha1.New()
 	hash.Write([]byte(password))
 
